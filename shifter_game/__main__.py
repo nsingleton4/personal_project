0d549299fddd510e50670bd87f5678aee7d6adcc
Nate <natesingleton12@gmail.com>
2026-04-29 17:03:37 -0400

    just having fun

---
+++ shifter_game/__main__.py
@@ -0,0 +1,34 @@
+from shifter_game.player.player_shifter import Shifter
+
+def character_input():
+    # making the initial input "False"
+    correct_input = False
+    # character name is blank initially
+    new_char = None
+    # this is where the inputs override the false tags above
+    while not correct_input:
+        input_name = input("\nWhat is your name?  >>>  ")
+        input_class = input("\nYou will either shift the world around you or be shifted by it (type ready to start)  >>>   ")
+        # if this class is druid, then it will return new_char
+        if input_class.lower() == "ready":
+            new_char = Shifter(name=input_name)
+            # this is what overrides the initial false tag based on the inputs
+            correct_input = True
+    return new_char
+
+# creates an object of character using the function
+char1 = character_input()
+
+# uses the object using character_input and the display sheet function of Druid to make the character sheet
+char1.display_sheet()
+
+# my_input_3 = input("\nBegin adventure? (yes or no) >>>  ")
+#
+# while True:
+#     if my_input_3.lower() == "yes":
+#         start_intro_dialogue()
+#     elif my_input_3.lower() not in {'quit', 'no'}:
+#         break
+#     else:
+#         my_input_3 = input("\n \"{}\" is not a valid entry. Begin adventure? (yes or no) >>>  ".format(my_input_3))
+# print(f"Your adventure ends. Come back when you're ready.")

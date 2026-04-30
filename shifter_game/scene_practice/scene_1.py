74698552852b9bf8a252d7f7776c65281ff793ee
Nate <natesingleton12@gmail.com>
2026-04-30 15:10:52 -0400

    just having fun

--- shifter_game/scene_practice/scene_1.py
+++ shifter_game/scene_practice/scene_1.py
@@ -1,16 +1,3 @@
-from shifter_game.player.player_shifter import Shifter
+from shifter_game.player.player_shifter import player
 
-# Example data for creating a Shifter instance
-shifter_data = {
-    'name': 'Goose',
-    'age': 21,
-    'level': 2,
-    'health': 100,
-    'experience': 15
-}
-
-# Create an instance of the Shifter class
-shifter = Shifter(**shifter_data)
-
-# Call the display_sheet method to print out the character's details
-shifter.display_sheet()
+print(player)

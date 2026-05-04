ff48aad60b408be14355a72f6f413ef6b94de461
Nate <natesingleton12@gmail.com>
2026-05-04 14:08:34 -0400

    fight scene

--- game/tutorial/scene1.py
+++ game/tutorial/scene1.py
@@ -5,22 +5,6 @@ from game.items.conventional_weapons import basic_spear
 from game.enemies.bandit import bandit
 from game.dice import roll_dice
 
-# def start_intro():
-#     current_node = attack["front_door"]
-#
-#     while True:
-#         print(current_node["text"])
-#
-#         for i, choice in enumerate(current_node["choice"]):
-#             print(f"{i + 1}: {choice['text']}")  # fixed quotes
-#
-#         user_input = int(input("Choose an option: ")) - 1
-#         selected_choice = current_node["choice"][user_input]
-#
-#         if selected_choice.get("action") == "Attack the bandit!":
-#             player_turn_tutorial_fight(player, bandit)
-#             break
-
 def scene_1(p1, p2=None):
     print(f"Hi, {p1["name"]}")
     print("\n\nYou walk out the front door and see a bandit trying to rob you")

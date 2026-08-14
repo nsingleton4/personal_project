87643b4d128e743605e3cbe3baf0d4dbb9f8b6e3
Nate <natesingleton12@gmail.com>
2026-08-14 15:28:14 -0500

    classes and such

--- att/items/clothes.py
+++ att/items/clothes.py
@@ -1,9 +1,22 @@
-basic_tunic = {
-    "name": "Rugged Tunic",
-    "defense": 12
-}
+class Armor:
+    def __init__(self, name=None, defense=None):
+        self.name = name
+        self.defense = defense
 
-rough_leathers = {
-    "name": "Rough Leathers",
-    "defense": 10
-}
+class Tunic(Armor):
+    def __init__(self, name=None, defense=None):
+        super().__init__(name, defense)
+        self.name = "Rough Tunic"
+        self.defense = 10
+
+class SteelPlate(Armor):
+    def __init__(self, name=None, defense=None):
+        super().__init__(name, defense)
+        self.name = "Steel Plate"
+        self.defense = 15
+
+class DragonPlate(Armor):
+    def __init__(self, name=None, defense=None):
+        super().__init__(name, defense)
+        self.name = "Pristine Dragon Plate"
+        self.defense = 20
--- att/items/weapons.py
+++ att/items/weapons.py
@@ -1,38 +1,29 @@
 from att.structural import roll
-basic_spear = {
-    "name": "Trusty Spear",
-    "attack_dice": "d20",
-    "damage_dice": "d6",
-    "attack_bonus": 2,
-    "damage_bonus": 1,
-    "scaling": "dexterity"
-}
 
-worn_longsword = {
-    "name": "Worn Longsword",
-    "attack_dice": "d20",
-    "damage_dice": "d6",
-    "attack_bonus": 0,
-    "damage_bonus": 0,
-    "scaling": "dexterity"
-}
 
 class TwoHanded:
-    def __init__(self, name = "", atk_dice=None, dmg_dice=None, level=None, abilities=None):
+    def __init__(self, name=None, atk_roll=None, dmg_roll=None, level=None, abilities=None):
         self.name = name
-        self.atk_dice = atk_dice
-        self.dmg_dice = dmg_dice
+        self.atk_roll = atk_roll
+        self.dmg_roll = dmg_roll
         self.level = level
         self.abilities = abilities
 
 class Spear(TwoHanded):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, name=None, atk_roll=None, dmg_roll=None, level=None, abilities=None):
+        super().__init__(name, atk_roll, dmg_roll, level, abilities)
+        self.atk_roll = roll(d20=True)
+        self.dmg_roll = roll(d10=True)
 
 class Greatsword(TwoHanded):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, name=None, atk_roll=None, dmg_roll=None, level=None, abilities=None):
+        super().__init__(name, atk_roll, dmg_roll, level, abilities)
+        self.atk_roll = roll(d20=True)
+        self.dmg_roll = (roll(d6=True) * 2)
 
 spear_basic = Spear()
+spear_basic.name = "Trusty Spear"
+print(spear_basic.name, spear_basic.atk_roll)
 
-print(spear_basic.name)
+big_sword = Greatsword()
+print(big_sword.dmg_roll)
